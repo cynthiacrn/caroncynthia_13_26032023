b83c3c6d2631fb125fb4376a2c9232fdd11d5322
Cynthia <caron.cynthia@outlook.fr>
2023-03-26 22:34:43 +0200

    update navigation

--- app/src/components/Navigation.jsx
+++ app/src/components/Navigation.jsx
@@ -1,8 +1,15 @@
-import { useSelector } from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import ArgentBankLogo from "../assets/images/argentBankLogo.png";
+import {Link} from "react-router-dom";
+import {logout} from "../store/reducers/auth";
 
 function Navigation() {
+  const dispatch = useDispatch()
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
+
+  function onLogout() {
+    dispatch(logout())
+  }
 
   return (
     <nav className="main-nav">
@@ -19,21 +26,21 @@ function Navigation() {
           isAuthenticated
             ? (
               <>
-                <a className="main-nav-item" href="/user">
+                <Link className="main-nav-item" to="/user">
                   <i className="fa fa-user-circle"></i>
                   Tony
-                </a>
-                <a className="main-nav-item" href="/">
+                </Link>
+                <button onClick={onLogout} className="main-nav-item">
                   <i className="fa fa-sign-out"></i>
                   Sign Out
-                </a>
+                </button>
               </>
             )
             : (
-              <a className="main-nav-item" href="/sign-in">
+              <Link to="/sign-in" className="main-nav-item">
                 <i className="fa fa-user-circle"></i>
                 Sign In
-              </a>
+              </Link>
             )
         }
       </div>
