33dd8e639ca53aed9daddcf4881cb695f73a2474
Cynthia <caron.cynthia@outlook.fr>
2023-03-26 22:25:55 +0200

    create login form

--- app/src/pages/SignIn.jsx
+++ app/src/pages/SignIn.jsx
@@ -1,7 +1,17 @@
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+import {useState} from "react";
 
 function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [remember, setRemember] = useState(false)
+
+  function onSubmit(event) {
+    event.preventDefault()
+    console.log({ email, password, remember })
+  }
+
   return (
     <>
       <Navigation />
@@ -9,22 +19,45 @@ function SignIn() {
       <main className="main bg-dark">
         <section className="sign-in-content">
           <i className="fa fa-user-circle sign-in-icon"></i>
+
           <h1>Sign In</h1>
+
           <form>
             <div className="input-wrapper">
-              <label htmlFor="username">Username</label
-              ><input type="text" id="username"/>
+              <label htmlFor="username">Username</label>
+              <input
+                id="username"
+                type="text"
+                value={email}
+                onInput={(event) => setEmail(event.target.value)}
+              />
             </div>
+
             <div className="input-wrapper">
-              <label htmlFor="password">Password</label
-              ><input type="password" id="password"/>
+              <label htmlFor="password">Password</label>
+              <input
+                id="password"
+                type="password"
+                value={password}
+                onInput={(event) => setPassword(event.target.value)}
+              />
             </div>
+
             <div className="input-remember">
-              <input type="checkbox" id="remember-me"/><label htmlFor="remember-me"
-            >Remember me</label
-            >
+              <input
+                id="remember-me"
+                type="checkbox"
+                checked={remember}
+                onChange={() => setRemember(!remember)}
+              />
+              <label htmlFor="remember-me">
+                Remember me
+              </label>
             </div>
-            <button className="sign-in-button">Sign In</button>
+
+            <button type="submit" onClick={onSubmit} className="sign-in-button">
+              Sign In
+            </button>
           </form>
         </section>
       </main>
